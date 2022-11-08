7b4bb286e28453f90cda9d26bb30282e6f50d75d
Ben Weekes <benweekes73@gmail.com>
2022-11-08 13:55:25 +0000

    update adapter to provide configurable limits on number of audio and video subscriptions. optional ability to drop subscriptions for people beyond a certain distance in the scene

--- agora-dialog-adapter.js
+++ agora-dialog-adapter.js
@@ -1,4 +1,4 @@
-import AgoraRTC  from 'agora-rtc-sdk-ng';
+import AgoraRTC from 'agora-rtc-sdk-ng';
 import { debug as newDebug } from "debug";
 import EventEmitter from "eventemitter3";
 
@@ -10,8 +10,29 @@ export const DIALOG_CONNECTION_CONNECTED = "dialog-connection-connected";
 export const DIALOG_CONNECTION_ERROR_FATAL = "dialog-connection-error-fatal";
 
 export class DialogAdapter extends EventEmitter {
+
   constructor() {
     super();
+
+    // If your Agora appId has tokens enabled then you can set a tokenAPI URL below to request a token
+    // To quickly run an AWS Lambda token service see https://github.com/BenWeekes/agora-rtc-lambda
+    // set Agora appId here
+
+    this.appId = "Your App Id Here"; 
+    
+    // set token server if tokens are enabled else null
+    this.tokenAPI = null; // e.g. "https://24volagzyvl2t3cziyxhiy7kpy0tdzke.lambda-url.us-east-1.on.aws/?channel="; 
+
+    this.limitSubscriptions = true;  // set to false to always subscribe to all available streams (or the host limit of your appid which has a default of 16)
+    this.maxAudioSubscriptions = 8;  // when more than this number of publishers are available then only the closest X neighbours will be subscribed to.
+    this.maxAudioDistanceApart = -1;  // only subscribe to audio of people within this distance in hubs scene (in any direction)  or set to -1 for no limit 
+    this.maxVideoSubscriptions = 6;  // when more than this number of publishers are available then only the closest X neighbours will be subscribed to.
+    this.maxVideoDistanceApart = -1;  // only subscribe to video of people within this distance in hubs scene (in any direction) or set to -1 for no limit 
+    this.processSubscriptonsAfter = 300; // time between subsequent subscription processes in ms (recommended 300 ms)
+
+    this.extension = null;
+    this.processor = null;
+    this.userid = null;
     this._micShouldBeEnabled = false;
     this._localMediaStream = null;
     this._pendingMediaRequests = new Map();
@@ -19,26 +40,22 @@ export class DialogAdapter extends EventEmitter {
     this.scene = null;
     this._serverParams = {};
     this._consumerStats = {};
+
     this._agora_client = null;
-
-    // If your Agora appId has tokens enabled then you can set a tokenAPI URL below to request a token
-    // To quickly run an AWS Lambda token service see https://github.com/BenWeekes/agora-rtc-lambda
-    // set Agora appId here
-    this.appId = "You AppId Here"; 
-    // set token server if tokens are enabled else null
-    this.tokenAPI=null;// "https://24volagzyvl2t3cziyxhiy7kpy0tdzke.lambda-url.us-east-1.on.aws/?channel="; 
-    this.virtualBackgroundInstance = null;
-    this.extension = null;
-    this.processor = null;
-    this.userid = null;
-
+    this._agoraUserMap = {};
     this.localTracks = {
       videoTrack: null,
       audioTrack: null
-    };
+    }
+
+    this._videoSubscriptions = {};
+    this._audioSubscriptions = {};
+    this._videoPublishers = {};
+    this._audioPublishers = {};
+    this._audioPubCount = 0; 
+    this._videoPubCount = 0;
   }
 
-  // public
   async connect({
     serverUrl,
     roomId,
@@ -57,68 +74,84 @@ export class DialogAdapter extends EventEmitter {
     var that = this;
 
     this._agora_client.on("user-joined", async (user) => {
-      console.info("user-joined "+ user.uid);
+      console.info("user-joined " + user.uid);
     });
 
     this._agora_client.on("user-published", async (user, mediaType) => {
-      let clientId = user.uid;
-      await that._agora_client.subscribe(user, mediaType);
+      var uid_string = user.uid.toString();
+      that._agoraUserMap[uid_string] = user;
       if (mediaType === 'audio') {
-        that.resolvePendingMediaRequestForTrack(clientId, user.audioTrack._mediaStreamTrack);
+        that._audioPublishers[uid_string] = user;
+        this._audioPubCount = this.getMapSize(this._audioPublishers);
       } else if (mediaType === 'video') {
-        that.resolvePendingMediaRequestForTrack(clientId, user.videoTrack._mediaStreamTrack);
+        that._videoPublishers[uid_string] = user;
+        this._videoPubCount = this.getMapSize(this._videoPublishers);
       }
-      that.emit("stream_updated", clientId, mediaType);
     });
 
     this._agora_client.on("user-unpublished", async (user, mediaType) => {
-      let clientId = user.uid;
-      that.closeRemote(clientId);
+      var uid_string = user.uid.toString();
+      if (mediaType === 'audio') {
+        delete that._audioPublishers[uid_string];
+        this._audioPubCount = this.getMapSize(this._audioPublishers);
+      } else if (mediaType === 'video') {
+        delete that._videoPublishers[uid_string];
+        this._videoPubCount = this.getMapSize(this._videoPublishers);
+      }
+    });
+
+    this._agora_client.on("user-left", async (user, mediaType) => {
+      var uid_string = user.uid.toString();
+      delete that._agoraUserMap[uid_string];
+      delete that._audioPublishers[uid_string];
+      this._audioPubCount = this.getMapSize(this._audioPublishers);
+      delete that._videoPublishers[uid_string];
+      this._videoPubCount = this.getMapSize(this._videoPublishers);
+
+      that.closeRemote(uid_string);
     });
 
 
     return new Promise((resolve, reject) => {
       (async () => {
-      try {
-        await this._joinRoom();
-        resolve();
-        this.emit(DIALOG_CONNECTION_CONNECTED);
-      } catch (err) {
-        reject(err);
-        this.emit(DIALOG_CONNECTION_ERROR_FATAL);
-      }
-    })()
+        try {
+          await this._joinRoom();
+          resolve();
+          this.emit(DIALOG_CONNECTION_CONNECTED);
+          this.manageSubscriptions();
+        } catch (err) {
+          reject(err);
+          this.emit(DIALOG_CONNECTION_ERROR_FATAL);
+        }
+      })()
     });
   }
 
   // private
   async _joinRoom() {
     // request token
-    if (this.tokenAPI!==null) {
-      let token_api=this.tokenAPI+this._roomId+"&uid="+this._clientId;
+    if (this.tokenAPI !== null) {
+      let token_api = this.tokenAPI + this._roomId + "&uid=" + this._clientId;
       try {
         const respJson = await fetch(`${token_api}`).then(r => r.json());
         //let uid = respJson.uid;
         let token = respJson.token;
         await this._agora_client.join(this.appId, this._roomId, token, this._clientId);
-    } catch (e) {
-      console.error("Error fetching/using Agora Token ", e);
-      return;
+      } catch (e) {
+        console.error("Error fetching/using Agora Token ", e);
+        return;
+      }
+    } else {
+      try {
+        await this._agora_client.join(this.appId, this._roomId, null, this._clientId);
+      } catch (e) {
+        console.error("Failed to join Agora ", e);
+        return;
+      }
     }
-  } else {
-    try {
-      await this._agora_client.join(this.appId, this._roomId, null, this._clientId);
-    } catch (e) {
-      console.error("Failed to join Agora ",e);
-      return;
-    } 
-  }
-
-    
     await this.setLocalMediaStream(this._localMediaStream);
   }
 
-  // private
   closeRemote(clientId) {
     const pendingMediaRequests = this._pendingMediaRequests.get(clientId);
     if (pendingMediaRequests) {
@@ -132,7 +165,6 @@ export class DialogAdapter extends EventEmitter {
     }
   }
 
-  // private
   resolvePendingMediaRequestForTrack(clientId, track) {
     const requests = this._pendingMediaRequests.get(clientId);
     if (requests && requests[track.kind]) {
@@ -146,13 +178,7 @@ export class DialogAdapter extends EventEmitter {
   }
 
   getAgoraUser(clientId) {
-    for (var u=0; u< this._agora_client._users.length; u++ )
-    {
-     if (this._agora_client._users[u].uid==clientId) {
-      return this._agora_client._users[u];
-     }
-    }
-    return null;
+    return this._agoraUserMap[clientId];
   }
 
   // public - returns promise 
@@ -160,16 +186,16 @@ export class DialogAdapter extends EventEmitter {
     let track;
     if (this._clientId === clientId) {
       // LOCAL USER
-      if (kind === "audio" &&  this.localTracks.audioTrack) {
+      if (kind === "audio" && this.localTracks.audioTrack) {
         track = this.localTracks.audioTrack._mediaStreamTrack;
       } else if (kind === "video" && this.localTracks.videoTrack) {
         track = this.localTracks.videoTrack._mediaStreamTrack;
       }
-    } else { 
+    } else {
       // REMOTE USERS
-      let user=this.getAgoraUser(clientId);
-      if (user && kind === "audio" &&  user.audioTrack && user.audioTrack._mediaStreamTrack) {
-        track =  user.audioTrack._mediaStreamTrack;
+      let user = this.getAgoraUser(clientId);
+      if (user && kind === "audio" && user.audioTrack && user.audioTrack._mediaStreamTrack) {
+        track = user.audioTrack._mediaStreamTrack;
       } else if (user && kind === "video" && user.videoTrack && user.videoTrack._mediaStreamTrack) {
         track = user.videoTrack._mediaStreamTrack;
       }
@@ -196,33 +222,31 @@ export class DialogAdapter extends EventEmitter {
 
   // public - void 
   async setLocalMediaStream(stream) {
-    
     await this._agora_client.unpublish();
-
     if (!stream) {
       return;
     }
     await Promise.all(
       stream.getTracks().map(async track => {
         if (track.kind === "audio") {
-          this.localTracks.audioTrack=await AgoraRTC.createCustomAudioTrack({
+          this.localTracks.audioTrack = await AgoraRTC.createCustomAudioTrack({
             mediaStreamTrack: stream.getAudioTracks()[0]
           });
           if (this.isMicEnabled()) {
             this.emit("mic-state-changed", { enabled: true });
-            await this._agora_client.publish( this.localTracks.audioTrack);
+            await this._agora_client.publish(this.localTracks.audioTrack);
           }
         } else if (track.kind === "video") {
-          this.localTracks.videoTrack=await AgoraRTC.createCustomVideoTrack({
-            mediaStreamTrack: stream.getVideoTracks()[0], bitrateMin: 600, bitrateMax: 1500 , optimizationMode: 'motion'
+          this.localTracks.videoTrack = await AgoraRTC.createCustomVideoTrack({
+            mediaStreamTrack: stream.getVideoTracks()[0], bitrateMin: 600, bitrateMax: 1500, optimizationMode: 'motion'
           });
-          if (this.localTracks &&this.localTracks.videoTrack) {
-            await this._agora_client.publish( this.localTracks.videoTrack);
+          if (this.localTracks && this.localTracks.videoTrack) {
+            await this._agora_client.publish(this.localTracks.videoTrack);
           }
         }
         this.resolvePendingMediaRequestForTrack(this._clientId, track);
       })
-    );  
+    );
     this._localMediaStream = stream;
   }
 
@@ -249,14 +273,14 @@ export class DialogAdapter extends EventEmitter {
   enableMicrophone(enabled) {
     if (!this.localTracks || !this.localTracks.audioTrack) {
       console.error("Tried to toggle mic but there's no mic.");
-      enabled=false;
+      enabled = false;
     } else {
       this.localTracks.audioTrack.setEnabled(enabled);
     }
     this.emit("mic-state-changed", { enabled: enabled });
   }
 
-   isMicEnabled() {
+  isMicEnabled() {
     return (this.localTracks && this.localTracks.audioTrack && this.localTracks.audioTrack._enabled);
   }
 
@@ -285,5 +309,163 @@ export class DialogAdapter extends EventEmitter {
 
   unblock(clientId) {
     document.body.dispatchEvent(new CustomEvent("unblocked", { detail: { clientId: clientId } }));
+  }
+
+  /*
+  Code to limit subscriptions based on distance
+  */
+
+  getMapSize(x) {
+    return Object.keys(x).length;
+  }
+
+
+   async addAudioSubsIfNotExisting(expected) {
+    Object.keys(expected).forEach(peerId => this.addAudioSubIfNotExisting(peerId))
+   }
+
+  async addAudioSubIfNotExisting(uid_string) {
+    if (this._audioSubscriptions[uid_string]) {
+      return;
+    }
+    var user = this._agoraUserMap[uid_string];
+    var client = this._agora_client;
+    this._audioSubscriptions[uid_string] = client;
+    var that = this;
+    await client.subscribe(user, 'audio').then(response => {
+      that.resolvePendingMediaRequestForTrack(user.uid, user.audioTrack._mediaStreamTrack);
+      that.emit("stream_updated", user.uid, 'audio');
+      console.info(" subscribe audio to "+user.uid);
+    }).catch(e => {
+      delete that._audioSubscriptions[uid_string];
+      console.error(e);
+    });
+  }
+
+  async removeAudioSubsIfNotExpected(expected) {
+    var that = this;
+    Object.keys(this._audioSubscriptions).forEach(async function (key) {
+      if (!expected[key]) {
+        var user = that._agoraUserMap[key];
+        var client = that._audioSubscriptions[key];
+        var prom = await client.unsubscribe(user, that.AUDIO);
+        console.info(" unsubscribe audio to "+key)
+        delete that._audioSubscriptions[key];
+      }
+    });
+  }
+
+  async addVideoSubsIfNotExisting(expected) {
+    Object.keys(expected).forEach(peerId => this.addVideoSubIfNotExisting(peerId))
+   }
+   
+  async addVideoSubIfNotExisting(uid_string) {
+    if (this._videoSubscriptions[uid_string]) {
+      return;
+    }
+    var user = this._agoraUserMap[uid_string];
+    var client = this._agora_client;
+    this._videoSubscriptions[uid_string] = client;
+    var that = this;
+    await client.subscribe(user, 'video').then(response => {
+      console.info(" subscribe video to "+user.uid)
+      that.resolvePendingMediaRequestForTrack(user.uid, user.videoTrack._mediaStreamTrack);
+      that.emit("stream_updated", user.uid, 'video');
+    }).catch(e => {
+      delete that._videoSubscriptions[uid_string];
+      console.error(e);
+    });
+  }
+
+  async removeVideoSubsIfNotExpected(expected) {
+    var that = this;
+    Object.keys(this._videoSubscriptions).forEach(async function (key) {
+      if (!expected[key]) {
+        var user = that._agoraUserMap[key];
+        var client = that._videoSubscriptions[key];
+        console.info(" unsubscribe video to "+key)
+        var prom = await client.unsubscribe(user, that.VIDEO);
+        delete that._videoSubscriptions[key];
+      }
+    });
+  }
+
+  async manageSubscriptions() {
+    try {
+      // pre
+      var _monitorStart=Date.now();      
+      this._manageSubscriptions();
+      var _monitorEnd=Date.now();
+      if (_monitorEnd-_monitorStart > this.processSubscriptonsAfter/2) {
+        console.warn("agora sub manage time took ",(_monitorEnd-_monitorStart));
+      }
+      // post
+    } catch (e) {
+      console.error(" manageSubscriptions ", e);
+    }
+
+    setTimeout(() => {this.manageSubscriptions()}, this.processSubscriptonsAfter);
+  }
+
+  async _manageSubscriptions() {
+
+    var expectedAudioSubs = {};
+    var expectedVideoSubs = {};
+
+    // only check distances when more users than slots
+    if (this.limitSubscriptions && (this._audioPubCount>this.maxAudioSubscriptions || this._videoPubCount>this.maxVideoSubscriptions || this.maxAudioDistanceApart>0 || this.maxVideoDistanceApart>0) ) {
+      const tmpWorldPos = new THREE.Vector3();
+      let self = AFRAME.scenes[0].querySelector("a-entity#avatar-rig").object3D;
+      let others = AFRAME.scenes[0].querySelectorAll("[avatar-audio-source]");
+      let distances = [];
+      // find distances 
+      for (var u = 0; u < others.length; u++) {
+        const peerId = await this.getOwnerId(others[u]);        
+        others[u].object3D.getWorldPosition(tmpWorldPos)
+        var distance = self.position.distanceTo(tmpWorldPos);
+        distances.push({ distance: distance, peerId: peerId });
+      }
+      distances.sort((a, b) => a.distance - b.distance);
+
+      var audioSubs=0;
+      for (var d = 0; (d < distances.length && audioSubs<this.maxAudioSubscriptions); d++) {
+        var peerId=distances[d].peerId;
+        if (this._audioPublishers[peerId] && (this.maxAudioDistanceApart<=0 || distances[d].distance<this.maxAudioDistanceApart)) {
+          audioSubs++;
+          expectedAudioSubs[peerId] = peerId;
+        //  console.log(" nearest " + peerId + " distance is "+distances[d].distance)
+        }
+      }
+
+      var videoSubs=0;
+      for (var d = 0; (d < distances.length && videoSubs<this.maxVideoSubscriptions); d++) {
+        var peerId=distances[d].peerId;
+        if (this._videoPublishers[peerId]  && (this.maxVideoDistanceApart<=0 || distances[d].distance<this.maxVideoDistanceApart) ) {
+          videoSubs++;
+          expectedVideoSubs[peerId] = peerId;
+        }
+      }
+    } else {
+      // copy all subs to expected 
+      expectedAudioSubs=this._audioPublishers;
+      expectedVideoSubs=this._videoPublishers;
+    }
+
+    // sync subscriptions
+    this.addAudioSubsIfNotExisting(expectedAudioSubs);
+    this.removeAudioSubsIfNotExpected(expectedAudioSubs);
+
+    this.addVideoSubsIfNotExisting(expectedVideoSubs);
+    this.removeVideoSubsIfNotExpected(expectedVideoSubs);
+  }
+
+  async getOwnerId(el) {
+    const networkedEl = await NAF.utils.getNetworkedEntity(el).catch(e => {
+      console.error(INFO_INIT_FAILED, INFO_NO_NETWORKED_EL, e);
+    });
+    if (!networkedEl) {
+      return null;
+    }
+    return networkedEl.components.networked.data.owner;
   }
 }
